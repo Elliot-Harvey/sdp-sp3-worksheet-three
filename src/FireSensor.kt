4c0a9b8b7e61bf6e7fdd5b93d37a28a9d951ab68
Elliot Harvey <eharve04@dcs.bbk.ac.uk>
2018-01-21 16:12:25 +0000

    EX1

--- src/FireSensor.kt
+++ src/FireSensor.kt
@@ -1,17 +1,33 @@
 class FireSensor() : Sensor {
+    val batteryTotal : Double = 100.0
+    val drainSpeed : Double = 0.1
+    var batteryLeft : Double = 0.0
+    val type : String = "Fire sensor"
+    val itsLocation : String = ""
+
     override fun isTriggered(): Boolean {
-        TODO("not implemented")
+        val RNG = java.util.Random()
+        if ((RNG.nextInt(100)+1 < 6)){
+            return true
+        }
+        return false
+
     }
 
     override fun getLocation(): String {
-        TODO("not implemented")
+        return itsLocation
     }
 
     override fun getSensortype(): String {
-        TODO("not implemented")
+        return type
     }
 
     override fun getBatteryPercentage(): Double {
-        TODO("not implemented")
+        return batteryLeft
+    }
+
+    fun drainBattery(){
+        batteryLeft-=batteryTotal*drainSpeed
+        if (batteryLeft < 0.0) {batteryLeft = 0.0}
     }
 }
